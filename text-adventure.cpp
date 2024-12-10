06d2ee66f8b0435eeac30fbba71d73ca849df410
MJ lynch <mjlynch35@yahoo.com>
2024-12-10 09:56:10 -0600

    Adding the opening comments and clearing the console when the user is ready

---
+++ text-adventure.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+    std::cout << "Welcome to the murder mystery game";
+    std::cout << "Press enter when you're ready";
+    std::cin.get();
+    system("clear");
+}
