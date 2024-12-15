c312fc9d6751366b852932cc2de638c49ddda7e2
MJ lynch <mjlynch35@yahoo.com>
2024-12-15 17:51:08 -0600

    Adde comments to fizzbuzz.cpp

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -4,18 +4,22 @@ int main()
 {
     for (int i = 1; i <= 100; i++)
     {
+        // Checking if the number is divisible by 3 and 5
         if (i % 3 == 0 && i % 5 == 0)
         {
             std::cout << "FizzBuzz\n";
         }
+        // Checking if number is divisble by 3
         else if (i % 3 == 0)
         {
             std::cout << "Fizz\n";
         }
+        // Checking if number is divisible by 5
         else if (i % 5 == 0)
         {
             std::cout << "Buzz\n";
         }
+        // If not
         else
         {
             std::cout << i << "\n";
