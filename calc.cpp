91b96e85af9a616b896369d0ff65d0c68922ce63
MJ lynch <mjlynch35@yahoo.com>
2024-12-15 18:00:51 -0600

    Added opening and allowing user to enter first and second number

---
+++ calc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void main()
+{
+    std::cout << "Welcome to my simple calculator\n";
+    std::cout << "Enter first number: \n";
+
+    int num1;
+    std::cin >> num1;
+
+    std::cout << "Enter second number: \n";
+
+    int num2;
+    std::cin >> num2;
+
+    std::cout << "Enter the operation (+, -, *, /): \n";
+
+    char op;
+}
+}
